fd8d8730e9f9465eef9c3f66fbbea30d4832ce66
Bruno <bruno_grb@hotmail.com>
2020-08-27 16:45:11 -0300

    adicionando arquivo Readme

--- Introduction - Web Development/Bloco 4/dia_1/script1.js
+++ Introduction - Web Development/Bloco 4/dia_1/script1.js
@@ -1,6 +1,23 @@
-console.log("Volvo");
-let a = "turma";
-let b = 20;
-let c = true;
-b = 25;
-c = false;
+let a = 5;
+let b = 9;
+
+
+// Adição
+
+let soma = a+b;
+
+// Subtração
+
+let subtracao = a-b;
+
+// Multiplicação
+
+let multiplicacao = a*b;
+
+// Divisão
+
+let divisao = a/b;
+
+// Módulo
+
+let modulo = a%b;
