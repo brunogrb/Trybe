f663f2473c201c32b03e82ae29f28f18cb0102c8
Bruno <bruno_grb@hotmail.com>
2020-08-27 19:12:15 -0300

    exercicio11 pronto

--- Introduction - Web Development/Bloco 4/dia_1/exercise11.js
+++ Introduction - Web Development/Bloco 4/dia_1/exercise11.js
@@ -1,17 +1,22 @@
-let salarioBruto = 1500.10;
-let descontoInss = 1;
-let descontoIr = 1;
+let salarioBruto = 3000;
+let salarioBase = 0;
+let descontoInss = 0;
+let descontoIr = 0;
 
 if(salarioBruto>5189.82) descontoInss = 570.88;
 else if(salarioBruto>2594.93) descontoInss = salarioBruto*0.11;
 else if(salarioBruto>1556.92)descontoInss = salarioBruto*0.09;
 else descontoInss=salarioBruto*0.08;
 
-if(salarioBruto<1903.98) descontoIr = 0;
-else if(salarioBruto<2826.65) descontoIr = 142.80 + salarioBruto*0.075;
-else if(salarioBruto<3751.05)descontoIr = 354.80 + salarioBruto*0.15;
-else if(salarioBruto<4664.68)descontoIr = 636.13 + salarioBruto*0.225;
-else descontoIr= 869.36 + salarioBruto*0.275;
+salarioBase = salarioBruto - descontoInss;
 
-console.log(descontoInss);
+if(salarioBase>4664.68) descontoIr = salarioBase*0.275 - 869,36;
+else if(salarioBase>=3751.06)descontoIr = salarioBase*0.225 - 636.13;
+else if(salarioBase>=2826.66) descontoIr = salarioBase*0.15 - 354.80;
+else if(salarioBase>=1903.99) descontoIr = salarioBase*0.075 - 142.80;
+else descontoIr = 0;
+
+salarioBase = salarioBase-descontoIr;
+
+console.log(salarioBase);
 
