4286f85f47d60908adbcdc57fe2cd911bf888f4e
Bruno <bruno_grb@hotmail.com>
2020-08-27 16:16:46 -0300

    testando JavaScript

--- Criar arquivos exercicios/criardorArquivos.sh
+++ Criar arquivos exercicios/criardorArquivos.sh
@@ -1,5 +1,5 @@
-for i in {1..12}
+for i in {1..5}
 do
-    echo "#!/bin/bash" >"exercise${i}.sh"
-    chmod +x "exercise${i}.sh"
+    echo "#!/bin/bash" >"script${i}.js"
+    chmod +x "script${i}.js"
 done
--- Introduction - Web Development/Bloco 4/dia_1/script1.js
+++ Introduction - Web Development/Bloco 4/dia_1/script1.js
@@ -1,1 +1,6 @@
-let carName = "Volvo"
+console.log("Volvo");
+let a = "turma";
+let b = 20;
+let c = true;
+b = 25;
+c = false;
