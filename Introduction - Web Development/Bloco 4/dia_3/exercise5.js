b9056aa80392eeb59ab2ec5d2a78d78d42562e5e
Bruno <bruno_grb@hotmail.com>
2020-08-31 15:22:43 -0300

    adicionando exercicio5 pronto

---
+++ Introduction - Web Development/Bloco 4/dia_3/exercise5.js
@@ -0,0 +1,22 @@
+let n = 5;                  //base of the pyramid
+
+for(let i=1; i<=n-1 ; i+=1){  //loop for printing the pyramid
+    let pyramid = "";
+    let k = 0;
+    for(let j=i;j<n;j+=1){  //printing spaces
+        pyramid += " ";
+    }
+                            //printing "*"
+    while(k != (2*i-1)){
+        if(k==0 || k==(2*i-2)) pyramid+="*";
+        else pyramid+=" ";
+
+        k+=1;
+    }
+    console.log(pyramid);
+}
+let piramid = "";
+for(let i=0;i<n;i+=1) { //printing pyramid base
+    piramid+="* ";
+}
+console.log(piramid);
