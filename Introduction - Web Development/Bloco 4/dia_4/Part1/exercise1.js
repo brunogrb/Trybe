3079be283800d9c1f2ffaabdf4895fa724a3c405
Bruno <bruno_grb@hotmail.com>
2020-09-01 13:55:48 -0300

    exercicio1 part1 pronto

--- Introduction - Web Development/Bloco 4/dia_4/Part1/exercise1.js
+++ Introduction - Web Development/Bloco 4/dia_4/Part1/exercise1.js
@@ -1,1 +1,7 @@
-let n = 5;
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+console.log("Welcome, " + info.personagem);
