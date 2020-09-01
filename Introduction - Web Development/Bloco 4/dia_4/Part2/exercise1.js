90a611ec1a4cb94943c824f6a26a37cd10a3b06a
Bruno <bruno_grb@hotmail.com>
2020-09-01 14:55:03 -0300

    exercicio1 part2 pronto

---
+++ Introduction - Web Development/Bloco 4/dia_4/Part2/exercise1.js
@@ -0,0 +1,16 @@
+function verificaPalindrome(palavra){
+  let retorno = true;
+  let palavra2 = "";
+
+  for(let i=palavra.length-1; i>=0;i-=1){
+    palavra2+=palavra[i];
+  }
+  for(let i=0; i<palavra.length;i+=1){
+    if (palavra2[i]!=palavra[i]){
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(verificaPalindrome("arara"));
