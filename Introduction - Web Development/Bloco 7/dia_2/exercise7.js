55a25ed4c205d73d8421fe48b891dec09077775d
Bruno <bruno_grb@hotmail.com>
2020-09-28 18:23:12 -0300

    exercicio7 finalizado

---
+++ Introduction - Web Development/Bloco 7/dia_2/exercise7.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+  turno: 'tarde',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function getValueByNumber (object, position) {
+  return Object.values(object)[position];
+}
+
+console.log(getValueByNumber(lesson1,2));
