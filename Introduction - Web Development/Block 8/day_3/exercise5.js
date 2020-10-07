8120785ba803d3bfcd911c4642c7d50513962ad4
Bruno <bruno_grb@hotmail.com>
2020-10-07 19:05:06 -0300

    exercicio4 pronto e iniciando implementacao exercicio 5

---
+++ Introduction - Web Development/Block 8/day_3/exercise5.js
@@ -0,0 +1,16 @@
+    
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+  
+}
+
+assert.deepStrictEqual(containsA(), 20);
