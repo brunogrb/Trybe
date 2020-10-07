a6a55f00c16061b6ba020b462a04a545f682bf93
Bruno <bruno_grb@hotmail.com>
2020-10-07 19:28:32 -0300

    exercicio5 pronto

--- Introduction - Web Development/Block 8/day_3/exercise5.js
+++ Introduction - Web Development/Block 8/day_3/exercise5.js
@@ -1,5 +1,6 @@
     
 const assert = require('assert');
+const { gzip } = require('zlib');
 
 const names = [
   'Aanemarie',  'Adervandes',   'Akifusa',
@@ -7,10 +8,10 @@ const names = [
   'Abeladerco', 'Adieidy',  'Alarucha',
 ];
 
-
-
 function containsA() {
-  
+  return names.reduce((store, value) => {
+    return store + value.match(/a/gi).length;
+  }, 0);
 }
 
 assert.deepStrictEqual(containsA(), 20);
